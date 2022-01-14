110511c22402be90bf1b5c840e979ccc20eac890
Pedro Sobrinho <pedrosobrinho.physics@gmail.com>
2022-01-14 19:51:58 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PyCacl
+
+Pedro H. N. Sobrinho (PSobrinho)
+
+Mail: pedrosobrinho.physics@gmail.com
+
+LinkedIn: https://www.linkedin.com/in/pedro-sobrinho-data/
+
+Instagram: https://www.instagram.com/phnsobrinho/?hl=pt-br
+
+Projeto experimental de desenvolvimento com python utilizando a biblioteca Kivy para construção de interface gráfica. 
+
+Fique a vontade, e não repara a bagunça :D
---
+++ main.py
@@ -0,0 +1,50 @@
+'''
+#############################################################
+##Projeto experimental de uma calculadora utilizando 'kivy'##
+#############################################################
+'''
+
+from kivy.app import App
+from kivy.lang import Builder
+
+GUI = Builder.load_file("screen.kv") # Instancia a interface a partir do arquivo .kv
+
+class pyCalc(App): # Cria a aplicação através da classe
+	def build(self):
+		return GUI # Carrega a interface
+
+	def getNumberA(self):
+		# Recupera o dado de entrada no campo 'numberA'
+		a = float(self.root.ids['numberA'].text)
+		return a
+
+	def getNumberB(self):
+		# Recupera o dado de entrada no campo 'numberB'
+		b = float(self.root.ids['numberB'].text)
+		return b
+
+	def sum(self, a, b):
+		# Função de soma
+		c = a + b
+		self.root.ids['result'].text = str(c)
+
+	def sub(self, a, b):
+		# Função de subtração
+		c = a - b
+		self.root.ids['result'].text = str(c)
+
+	def mult(self, a, b):
+		# Função de multiplicação
+		c = a * b
+		self.root.ids['result'].text = str(c)
+
+	def div(self, a, b):
+		# Função de divisão
+		if not b == 0:
+			c = a / b
+			self.root.ids['result'].text = str(c)
+		else:
+			self.root.ids['result'].text = 'Invalid Operation'
+
+
+pyCalc().run()
